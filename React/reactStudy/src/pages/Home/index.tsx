f22d0a259352a759f6c6afd911ad04c45e32a906
Miqstelles <90120169+Miqstelles@users.noreply.github.com>
2023-04-14 22:24:46 -0300

    initial commit: react fundamentals

---
+++ React/reactStudy/src/pages/Home/index.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './style.css'
+
+export function Home() {
+    return (
+        <div className="container">
+            <h1>LISTA</h1>
+            <input type="" placeholder='Digite o nome...' />
+            <button type="button">Adicionar</button>
+        </div>
+    )
+}
