39a80bec9929082f642ba8ebc6f86ad485e9b143
Miqstelles <90120169+Miqstelles@users.noreply.github.com>
2023-04-19 22:12:11 -0300

    object values

---
+++ JavaScript/ES8/Object Values/object_values_parte1.js
@@ -0,0 +1,15 @@
+let objeto = {
+    nome: 'Miqs Telles',
+    profissional: {
+        site: 'miqs.com.br',
+        profissao: 'programador',
+    },
+    hobbies: [
+        { id: 1, hobby: 'Acordar' },
+        { id: 2, hobby: 'Dormir' },
+    ],
+    pais: 'Brasil'
+}
+
+console.log(objeto)
+console.log(Object.values(objeto))
