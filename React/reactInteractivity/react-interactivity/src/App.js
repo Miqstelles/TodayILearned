a7ee50096584d635105ab9af662fccdc338fde10
Miqstelles <90120169+Miqstelles@users.noreply.github.com>
2023-05-17 19:05:15 -0300

    updating objects in state

--- React/reactInteractivity/react-interactivity/src/App.js
+++ React/reactInteractivity/react-interactivity/src/App.js
@@ -1,22 +1,96 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [pessoa, setPessoa] = useState({
+    nome: 'Niki de Saint Phalle',
+    arte: {
+      titulo: 'Blue Nana',
+      cidade: 'Hamburg',
+      imagem: 'https://i.imgur.com/Sd1AgUOm.jpg',
+    }
+  });
+
+  function handleNameChange(e) {
+    setPessoa({
+      ...pessoa,
+      nome: e.target.value
+    });
+  }
+
+  function handleTitleChange(e) {
+    setPessoa({
+      ...pessoa,
+      arte: {
+        ...pessoa.arte,
+        titulo: e.target.value
+      }
+    });
+  }
+
+  function handleCityChange(e) {
+    setPessoa({
+      ...pessoa,
+      arte: {
+        ...pessoa.arte,
+        cidade: e.target.value
+      }
+    });
+  }
+
+  function handleImageChange(e) {
+    setPessoa({
+      ...pessoa,
+      arte: {
+        ...pessoa.arte,
+        imagem: e.target.value
+      }
+    });
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <label>
+          Nome:
+          <input
+            value={pessoa.nome}
+            onChange={handleNameChange}
+          />
+        </label>
+        <label>
+          Titulo:
+          <input
+            value={pessoa.arte.titulo}
+            onChange={handleTitleChange}
+          />
+        </label>
+        <label>
+          Cidade:
+          <input
+            value={pessoa.arte.cidade}
+            onChange={handleCityChange}
+          />
+        </label>
+        <label>
+          Imagem:
+          <input
+            value={pessoa.arte.imagem}
+            onChange={handleImageChange}
+          />
+        </label>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <i>{pessoa.arte.titulo}</i>
+          {' por '}
+          {pessoa.nome}
+          <br />
+          (localizado em {pessoa.arte.cidade})
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img
+          src={pessoa.arte.imagem}
+          alt={pessoa.arte.titulo}
+          width={'800px'}
+        />
       </header>
     </div>
   );
