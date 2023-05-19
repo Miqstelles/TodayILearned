052ec5af9ea7ef4ba9c5392b3c31650fac5b238e
Miqstelles <90120169+Miqstelles@users.noreply.github.com>
2023-05-18 23:50:20 -0300

    trimStart trimEnd

---
+++ JavaScript/ES10/trimStart trimEnd/trimStart_trimEnd.js
@@ -0,0 +1,7 @@
+let texto = '        teste         '
+
+console.log('.' + texto + '.')
+console.log('.' + texto.trimEnd().trimStart() + '.')
+
+console.log('.' + texto + '.')
+console.log('.' + texto.trimLeft().trimRight() + '.')
