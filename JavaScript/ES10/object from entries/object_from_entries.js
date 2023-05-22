6aa92f1599f1f005cdeff6488bc73111c64b82c1
Miqstelles <90120169+Miqstelles@users.noreply.github.com>
2023-05-22 18:04:19 -0300

    object fromEntries

---
+++ JavaScript/ES10/object from entries/object_from_entries.js
@@ -0,0 +1,13 @@
+let dados = [
+    ['nome', 'Miqs Telles'],
+    ['profisional', [
+        ['site', 'miq.com.br'],
+        ['profissão', 'programador']
+    ]],
+    ['hobbies', ['jogar', 'voar']],
+    ['pais', 'brasil']
+]
+
+console.log(Object.fromEntries(dados))
+
+// objetos: chave e valor
